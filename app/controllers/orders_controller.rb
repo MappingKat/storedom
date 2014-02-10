5a49d5e5c155aa57c759e167d5eb0ff90d6c3bec
Katrina Engelsted <katrina@engelsted.co>
2014-02-10 10:40:39 -0700

    WIP admin user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only [:index, :show]
 
   def index
-    @orders = Order.all
+    if current_user.admin?
+      @orders = Order.all
+    else 
+      # redirect_to order_path(current_user)
+      @orders = current_user.orders
   end
 
   def show
+    if current_user == User.find_by_id(Order.find(params[:id][:user_id])
     @order = Order.find(params[:id])
+  else
+    redirect_to order_path(current_user)
   end
 end
